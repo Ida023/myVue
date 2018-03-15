6f854eaadb38ccca11c1bdc0fda4287ac3d035d3
Ida023 <13603035423@163.com>
2018-03-15 11:54:23 +0800

    static

---
+++ static/webserver.config.js
@@ -0,0 +1,110 @@
+/*exports.getRmsUrl=function(){
+  return 'http://172.20.3.153:80/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.153:80/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.153:80/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.153:80/erp_rms/';
+}*/
+/*exports.getRmsUrl=function(){
+  return 'http://172.20.3.239/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.239/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.239/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.239/erp_rms/';
+}*/
+/*
+exports.getRmsUrl=function(){
+  return 'http://172.20.3.234/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.234/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.234/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.234/erp_rms/';
+}
+*/
+
+/*exports.getRmsUrl=function(){
+  return 'http://172.20.3.205/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.205/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.205/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.205/erp_rms/';
+}*/
+/*
+ exports.getRmsUrl=function(){
+ return 'http://172.20.3.205/erp_rms/';
+ }
+
+ exports.getScmUrl=function(){
+ return 'http://172.20.3.205/erp_rms/';
+ }
+
+ exports.getSmsUrl=function(){
+ return 'http://172.20.3.205/erp_rms/';
+ }
+
+ exports.getLwmsUrl=function(){
+ return 'http://172.20.3.205/erp_rms/';
+ }
+exports.getRmsUrl=function(){
+  return 'http://172.20.3.152/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.152/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.152/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.152/erp_rms/';
+}
+*/
+exports.getRmsUrl=function(){
+  return 'http://172.20.3.219/erp_rms/';
+}
+
+exports.getScmUrl=function(){
+  return 'http://172.20.3.219/erp_rms/';
+}
+
+exports.getSmsUrl=function(){
+  return 'http://172.20.3.219/erp_rms/';
+}
+
+exports.getLwmsUrl=function(){
+  return 'http://172.20.3.219/erp_rms/';
+}
